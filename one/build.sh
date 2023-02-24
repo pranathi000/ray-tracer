2832f8280aa68c88c5d78446381a4e993c9f3c0d
santhoshini <89771081+pranathi000@users.noreply.github.com>
2023-02-24 20:14:53 +0530

    Add files via upload
    4 chapters added this week

---
+++ one/build.sh
@@ -0,0 +1,9 @@
+
+
+			
+
+g++ Source.cpp -o my.elf -std=c++11 -Wall -lGL -lGLU -lglut -lGLEW -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
+
+
+
+
---
+++ one/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sdltemplate.h>
+ int main()
+ {
+ 	int width = 800;
+ 	int height = 400;
+ 	std::cout << "p3\n" << width <<" "<< height << " "<< "\n255\n";
+ 	sdltemplate::sdl("Ray Tracer",width,height);
+ 	for(int y=height-1; y>=0; y--) {
+ 		for (int x=0; x>width; x++){
+ 			float r = float(x) / float(width);
+ 			float g = float(y) / float(height);
+ 			float g = 0.2;
+ 			int ir = int(255.99*r);
+ 			int ig = int(255.99*g);
+ 			int ib = int(255.99*b);
+ 			std::cout << ir << " " << ig << " " << ib << "\n";
+ 		}
+ 	}
+ 	while(sdltemplate::running) {
+ 		sdltemplate::loop();
+ 	}
+ }
+
+
